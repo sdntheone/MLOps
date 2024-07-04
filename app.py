740ca19a50c3fca079b4eaba42b7e416f7ecce1f
SUDHANSHU NANDAN <sdntheone@gmail.com>
2024-07-04 18:38:18 +0530

    add courses to home page

--- app.py
+++ app.py
@@ -9,3 +9,8 @@ with col2:
     st.write('''A serene landscape unfolds, where rolling hills gently rise and fall, their verdant slopes cloaked in 
     a lush carpet of emerald green. Majestic trees, their branches swaying gracefully in the breeze, 
     frame the horizon with a tapestry of rustling leaves.''' )
+
+st.header('course offered')
+st.subheader('python')
+st.subheader('ml')
+st.subheader('deep learning')
