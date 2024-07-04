0ff1f329196c4ed6b72b273bd333f2ae23837ba0
SUDHANSHU NANDAN <sdntheone@gmail.com>
2024-07-04 23:55:29 +0530

    replace header image

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ st.title('futureai')
 col1,col2=st.columns(2)
 
 with col1:
-    st.image('scenery.jpg')
+    st.image('mor.jpg')
 with col2:
     st.write('''A serene landscape unfolds, where rolling hills gently rise and fall, their verdant slopes cloaked in 
     a lush carpet of emerald green. Majestic trees, their branches swaying gracefully in the breeze, 
